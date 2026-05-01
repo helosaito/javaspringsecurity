4fc28fd2da37ff0b2cfaf3f0f59c5357ebb1a974
unknown <helo.saito@outlook.co>
2026-04-30 22:19:14 -0300

    enum criado

---
+++ src/main/java/com/JavaSpringSecurity/demo/Models/User.java
@@ -0,0 +1,59 @@
+package com.JavaSpringSecurity.demo.Models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String password;
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
---
+++ src/main/java/com/JavaSpringSecurity/demo/enuns/userEnum.java
@@ -0,0 +1,9 @@
+package com.JavaSpringSecurity.demo.enuns;
+
+public enum userEnum {
+    ADIM("ADIM"),
+    USER ("USER");
+
+    private String value;
+    userEnum(String value) {}
+}
